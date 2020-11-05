2ab5d1462fb49951d660df257ea5d8a86d3d570d
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2020-11-04 21:12:08 -0300

    envio do desafio da aula 01

--- microsservicos-aula-1/a/a.go
+++ microsservicos-aula-1/a/a.go
@@ -2,39 +2,38 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/hashicorp/go-retryablehttp"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 type Result struct {
 	Status string
 }
 
-func main() {
+func main(){
 	http.HandleFunc("/", home)
 	http.HandleFunc("/process", process)
 	http.ListenAndServe(":9090", nil)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request){
 	t := template.Must(template.ParseFiles("templates/home.html"))
 	t.Execute(w, Result{})
 }
 
-func process(w http.ResponseWriter, r *http.Request) {
-
+func process(w http.ResponseWriter, r *http.Request){
+	//log.Println(r.FormValue("coupon"))
+	//log.Println(r.FormValue("cc-number"))
 	result := makeHttpCall("http://localhost:9091", r.FormValue("coupon"), r.FormValue("cc-number"))
-
 	t := template.Must(template.ParseFiles("templates/home.html"))
 	t.Execute(w, result)
 }
 
-func makeHttpCall(urlMicroservice string, coupon string, ccNumber string) Result {
-
+func makeHttpCall(urlMicroservice string , coupon string, ccNumber string) Result {
 	values := url.Values{}
 	values.Add("coupon", coupon)
 	values.Add("ccNumber", ccNumber)
@@ -44,21 +43,18 @@ func makeHttpCall(urlMicroservice string, coupon string, ccNumber string) Result
 
 	res, err := retryClient.PostForm(urlMicroservice, values)
 	if err != nil {
-		result := Result{Status: "Servidor fora do ar!"}
+		result := Result{Status: "Servidor ta caido"}
 		return result
 	}
-
 	defer res.Body.Close()
 
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal("Error processing result")
+		log.Fatal("error processing result")
 	}
 
 	result := Result{}
-
 	json.Unmarshal(data, &result)
 
 	return result
-
 }
